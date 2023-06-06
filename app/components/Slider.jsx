dd7d4a400b7d6469de6f3d9b2b6402109dd79bff
codeoz <codeoz@proton.me>
2023-06-07 00:10:50 +0300

    iconlar eklendi

---
+++ app/components/Slider.jsx
@@ -0,0 +1,109 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+import {
+  AiOutlineMenu,
+  AiOutlineHome,
+  AiOutlineProject,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+import { GrProjects } from "react-icons/gr";
+
+export default function Slider() {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div>
+      {/* hamburger icon */}
+      <AiOutlineMenu
+        className="absolute top-4 right-4 z-[99] md:hidden cursor-pointer"
+        onClick={handleNav}
+      />
+      {
+        // hamburger iconun icindeki menuler
+        nav ? (
+          <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center md:hidden">
+            <Link
+              href="/main"
+              className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-200"
+            >
+              <AiOutlineHome size={20} />
+              <span className="pl-4">Home</span>
+            </Link>
+            <Link
+              href="/work"
+              className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-200"
+            >
+              <GrProjects size={20} />
+              <span className="pl-4">Work</span>
+            </Link>
+            <Link
+              href="/projects"
+              className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-200"
+            >
+              <AiOutlineProject size={20} />
+              <span className="pl-4">Projects</span>
+            </Link>
+            <Link
+              href="#main"
+              className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-200"
+            >
+              <BsPerson size={20} />
+              <span className="pl-4">Resume</span>
+            </Link>
+            <Link
+              href="/contact"
+              className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-200"
+            >
+              <AiOutlineMail size={20} />
+              <span className="pl-4">Contact</span>
+            </Link>
+          </div>
+        ) : (
+          ""
+        )
+      }
+      {/* hamburger iconun disindaki menuler, anasayfadaki */}
+      <div className="hidden md:block fixed top-[25%] z-10">
+        <div className="flex flex-col">
+          <Link
+            href="/main"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-300"
+          >
+            <AiOutlineHome size={20} />
+          </Link>
+          <Link
+            href="/work"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-300"
+          >
+            <GrProjects size={20} />
+          </Link>
+          <Link
+            href="/projects"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-300"
+          >
+            <AiOutlineProject size={20} />
+          </Link>
+          <Link
+            href="#main"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-300"
+          >
+            <BsPerson size={20} />
+          </Link>
+          <Link
+            href="/contact"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 hover:scale-110 ease-in duration-300"
+          >
+            <AiOutlineMail size={20} />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
